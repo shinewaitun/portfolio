ffbab7307a35f0e4ed7131a920bac83de4e3ed4d
ShineWaiTun <shinewaitun49@gmail.com>
2025-06-06 14:07:09 +0700

    remove ripple effects

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -27,54 +27,10 @@ const languageSkills = [
 ];
 
 export default function HomePage() {
-  useEffect(() => {
-    const jqueryScript = document.createElement("script");
-    jqueryScript.src =
-      "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js";
-    jqueryScript.async = true;
-
-    const rippleScript = document.createElement("script");
-    rippleScript.src =
-      "https://cdn.jsdelivr.net/npm/jquery.ripples@0.6.3/dist/jquery.ripples.min.js";
-    rippleScript.async = true;
-
-    document.body.appendChild(jqueryScript);
-
-    jqueryScript.onload = () => {
-      document.body.appendChild(rippleScript);
-      rippleScript.onload = () => {
-        // @ts-ignore
-        const $ripple = window.$("#ripple");
-        // @ts-ignore
-        $ripple.ripples({
-          resolution: 512,
-          dropRadius: 20,
-          perturbance: 0.04,
-        });
-
-        $ripple.on("click", function (e: MouseEvent) {
-          const rect = (e.currentTarget as HTMLElement).getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-
-          // @ts-ignore
-          $ripple.ripples("drop", x, y, 20, 0.04);
-        });
-      };
-    };
-
-    return () => {
-      rippleScript.remove();
-      jqueryScript.remove();
-    };
-  }, []);
   return (
     <div>
       <HeroJumbotron />
-      <div
-        id="ripple"
-        className="bg-sea-gradient relative min-h-screen flex flex-col gap-5 md:gap-16 items-start justify-center overflow-hidden md:pt-20"
-      >
+      <div className="bg-sea-gradient relative min-h-screen flex flex-col gap-5 md:gap-16 items-start justify-center overflow-hidden md:pt-20">
         <motion.div
           id="about"
           initial={{ opacity: 0, y: 40 }}
